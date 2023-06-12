c650420370c52132818e6d009580c727db379d22
Ibeh Leonard <obinnaibeh42@gmail.com>
2023-06-13 00:37:36 +0100

    Refactored code

--- script.js
+++ script.js
@@ -258,3 +258,18 @@ const calcAv = function (data) {
 };
 
 calcAv(data1);
+
+// A to convert the first title to uppercase.
+const convertToUppercase = function (title) {
+  const exception = ["a", "an", "and", "the", "but", "or", "on", "in", "with"];
+  const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+
+  const titleCase = title
+    .toLowerCase()
+    .split(" ")
+    .map((word) => (exception.includes(word) ? word : capitalize(word)))
+    .join(" ");
+  return capitalize(titleCase);
+};
+
+console.log(convertToUppercase("the boy is a goat FUll"));
